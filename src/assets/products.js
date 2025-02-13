1b135dccd0140050b1af62f6801155785726d1b8
Nami-Kim-91 <nami910529@gmail.com>
2025-02-13 14:40:24 +0900

    update page

---
+++ src/assets/products.js
@@ -0,0 +1,8 @@
+export const products = [
+  {id:1, name: '여름원피스' , price:20000, image:'./images/img-1.jpg', category: 'Best'},
+  {id:2, name: '가을셋업' , price:8000, image:'./images/img-2.jpg', category: 'New'},
+  {id:3, name: '모나미세트' , price:11000, image:'./images/img-3.jpeg', category: '세트'},
+  {id:4, name: '데님바지' , price:7000, image:'./images/img-4.png', category: '하의'},
+  {id:5, name: '무지티셔츠' , price:5000, image:'./images/img-5.jpg', category: '상의'}
+];
+export const categories = ['전체','Best','New','세트','하의','상의'];
